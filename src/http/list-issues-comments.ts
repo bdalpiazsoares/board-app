b63439a1e6b7dfdbdd24cbb1957374f258ba24b3
Bernardo <bdalpiazsoares@gmail.com>
2026-08-05 19:55:57 -0300

    feat: comments cache

--- src/http/list-issues-comments.ts
+++ src/http/list-issues-comments.ts
@@ -1,13 +1,17 @@
 import { CommentsListResponseSchema } from '@/api/routes/list-issue-comments'
 import { clientEnv } from '@/env'
-import { setTimeout } from 'timers/promises'
+import { cacheLife, cacheTag } from 'next/cache'
 
 interface ListIssueCommentsParams {
   issueId: string
 }
 
 export async function listIssueComments({ issueId }: ListIssueCommentsParams) {
-  await setTimeout(2000)
+  'use cache'
+
+  cacheLife('minutes')
+  cacheTag(`issue-comments-${issueId}`)
+
   const url = new URL(
     `/api/issues/${issueId}/comments`,
     clientEnv.NEXT_PUBLIC_API_URL
